5a492932815ee618dadc6e325c24aa424b983a17
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-29 07:38:48 -0400

    Update build.gradle.kts

--- HighlightOwnMessages/build.gradle.kts
+++ HighlightOwnMessages/build.gradle.kts
@@ -1,2 +1,10 @@
-version = "1.1.2"
+version = "1.1.3"
 description = "Customize how messages from yourself look."
+aliucord.changelog.set(
+        """
+        # 1.1.3
+        * added colour pickers to make it easier to get the color
+        * removed input box(es) for the colours to now only use colour pickers
+        * added support for foreground colour (text)
+        """.trimIndent()
+)
